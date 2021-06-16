36bbc89f6c779ddab2aa277b5fa26c76312f9c4c
ak-bear <85649865+ak-bear@users.noreply.github.com>
2021-06-16 11:20:04 +0300

    final v2
    round всё же очевидней int

--- module_0/ugad.py
+++ module_0/ugad.py
@@ -28,7 +28,7 @@ def score_game(game_core):
     random_array = np.random.randint(1,101, size=(1000))
     for number in random_array:
         count_ls.append(game_core(number))
-    score = int(np.mean(count_ls))
+    score = round(np.mean(count_ls))
     print("Данный алгоритм угадывает число в среднем за ", score, " попыток")
     return(score)
 
