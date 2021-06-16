489d5548f32eb699ba0c95ee46830dfb0100d17b
ak-bear <85649865+ak-bear@users.noreply.github.com>
2021-06-16 10:34:25 +0300

    final version

--- module_0/ugad.py
+++ module_0/ugad.py
@@ -29,7 +29,7 @@ def score_game(game_core):
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
-    print("Ваш алгоритм угадывает число в среднем за ", score, " попыток")
+    print("Данный алгоритм угадывает число в среднем за ", score, " попыток")
     return(score)
 
 import numpy as np
